6fea30bd198e7422df11c833ed7113c55da9de94
VSDya <vdyadyanin@yandex.ru>
2023-02-25 21:09:37 +0300

    add Task01 (является ли 1ое число квдратом 2го)

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿double numberA,
+    numberB;
+Console.WriteLine("Введите большое целое число");
+numberA = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число для проверки");
+numberB = double.Parse(Console.ReadLine());
+double index = 1;
+if (index == numberA / (numberB * numberB))
+{
+    Console.WriteLine("Квадратом является");
+}
+else
+{
+    Console.WriteLine("Квадратом не является");
+}
