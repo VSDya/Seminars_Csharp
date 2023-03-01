8d6df9b3dca976208eebc99b215281dce6db6dac
VSDya <vdyadyanin@yandex.ru>
2023-03-01 22:13:29 +0300

    Всё сломалось((

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -17,3 +17,33 @@ else if (number == 6)
 else if (number == 7)
     Console.WriteLine("Воскресенье");
 else Console.Write("Неверный номер");
+
+// Console.WriteLine("Введите число от 1 до 7");
+// string day = Console.ReadLine();
+// switch (day)
+// {
+//     case "1":
+//         Console.WriteLine("Понедельник");
+//         break;
+//     case "2":
+//         Console.WriteLine("Вторник");
+//         break;
+//     case "3":
+//         Console.WriteLine("Среда");
+//         break;
+//     case "4":
+//         Console.WriteLine("Четверг");
+//         break;
+//     case "5":
+//         Console.WriteLine("Пятница");
+//         break;
+//     case "6":
+//         Console.WriteLine("Суббота");
+//         break;
+//     case "7":
+//         Console.WriteLine("Воскресенье");
+//         break;
+//     default:
+//         Console.WriteLine("Вы ввели некорректное число");
+//         break;
+// }
