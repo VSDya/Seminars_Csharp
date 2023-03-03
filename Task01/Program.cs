3275082ef6572a50f73b3a100439890beb474325
VSDya <vdyadyanin@yandex.ru>
2023-03-03 12:50:19 +0300

    fixed Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine("Введите первое целое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число для проверки");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 / (number2 * number2) == 1)
+if (number1 == number2 * number2)
 {
     Console.WriteLine("Квадратом является");
 }
