40675fff5d000abe84b93a877469ebf2512389ba
VSDya <vdyadyanin@yandex.ru>
2023-03-07 13:46:02 +0300

    Add homework Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число натуральное число (N)");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (numberN > index)
+{
+    Console.Write($"{index + 1}, ");
+    index = index + 2;
+}
