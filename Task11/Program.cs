791911803ee0b629da673b31ef59582261afc2f0
VSDya <vdyadyanin@yandex.ru>
2023-03-09 21:02:52 +0300

    Add fix Task11 - метод

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,11 +5,24 @@
 // 782 -> 72
 // 918 -> 98
 
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine(number);
+
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int result = firstDigit * 10 + thirdDigit;
+
+// Console.Write(result);
+
+int DelDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
 int number = new Random().Next(100, 1000);
-Console.WriteLine(number);
-
-int firstDigit = number / 100;
-int thirdDigit = number % 10;
-int result = firstDigit * 10 + thirdDigit;
-
-Console.Write(result);
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+int newNumber = DelDigit(number);
+Console.WriteLine($"Новое число -> {newNumber}");
