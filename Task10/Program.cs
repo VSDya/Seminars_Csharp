63caf33120e180edc6d7b9f95069b61a1a2981d5
VSDya <vdyadyanin@yandex.ru>
2023-03-09 23:26:01 +0300

    Add homework Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int newNumber = DelDigit(number);
+Console.WriteLine($"Новое число -> {newNumber}");
+
+int DelDigit(int num)
+{
+    int secondDigit = num / 10;
+    int result = secondDigit % 10;
+    return result;
+}
