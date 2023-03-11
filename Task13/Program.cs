e87af522cf2ea80621d49024e91b1c99dcc2b8f6
VSDya <vdyadyanin@yandex.ru>
2023-03-11 15:27:55 +0300

    Add homework Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,3 +6,18 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number /= 10; // number = number / 10
+    }
+    number = number % 10;
+    Console.WriteLine($"Третья цифра числа {number}");
+}
