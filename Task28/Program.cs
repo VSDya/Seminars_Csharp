96bb0785d9e26aa189bff6e7546947fcfee033bc
VSDya <vdyadyanin@yandex.ru>
2023-03-20 12:16:58 +0300

    Add Task28 произведение чисел от 1 до N

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int productNumbers = ProductNumbers(number);
+Console.WriteLine($"Произведение всех чисел от 1 до {number} = {productNumbers}");
+
+int ProductNumbers(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked // оператор который проверяет переполнение типа
+        {
+            result *= i; // resul = result * i;   
+        }
+    }
+    return result;
+}
