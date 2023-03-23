d276a50c4b5c936480948f0183781d27817c1dea
VSDya <vdyadyanin@yandex.ru>
2023-03-23 10:57:44 +0300

    Add homework Task34 количество четных чисел в массиве

---
+++ Task34/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: 
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// 2. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int FindColEvenNumber(int[] arr)
+{
+    int sumNumbers = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) sumNumbers++;
+    }
+    return sumNumbers;
+}
+
+int[] array = CreateArrayRndInt(4, 100, 999);
+PrintArray(array);
+int findColEvenNumber = FindColEvenNumber(array);
+Console.Write($"-> {findColEvenNumber}");
