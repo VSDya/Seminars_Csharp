2f2d0ebc862d77e5e5e6495e37e1e97c13846315
VSDya <vdyadyanin@yandex.ru>
2023-03-25 21:30:33 +0300

    Add Task48 двумерный массив по формуле: Amn =  m+n

---
+++ Task48/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] CreateMatrixByIndex(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4} "); // 3} отступ между элементами
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] matrix = CreateMatrixByIndex(3, 4);
+PrintArray(matrix);
