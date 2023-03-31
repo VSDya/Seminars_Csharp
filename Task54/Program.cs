0b84578d1610b94e9c46d801924109bd2dd188bc
VSDya <vdyadyanin@yandex.ru>
2023-03-31 16:05:10 +0300

    Add homework Task54 строки по убыванию

---
+++ Task54/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void RowsInDescendingOrder(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                if (matrix[i, j] > matrix[i, k])
+                {
+                    int min = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = min;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 4, 0, 20);
+Console.WriteLine($"Исходная матрица: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+RowsInDescendingOrder(matrix);
+Console.WriteLine($"Измененная матрица: ");
+PrintMatrix(matrix);
